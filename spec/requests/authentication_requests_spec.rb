89410f9e47b8dd44a696b928d703a46111bf35d8
Attila <hun.ati500@gmail.com>
2013-04-22 22:52:10 +0200

    Add user following

--- spec/requests/authentication_requests_spec.rb
+++ spec/requests/authentication_requests_spec.rb
@@ -11,19 +11,31 @@ describe "Authentication Requests" do
       describe "in the Users controller" do
         describe "submitting to the update action" do
           before { put user_path(user) }
-          specify { should redirect_to(signin_path) }
+          it { should redirect_to(signin_path) }
         end
       end
 
       describe "in the Microposts controller" do
         describe "submitting to the create action" do
           before { post microposts_path }
-          specify { should redirect_to(signin_path) }
+          it { should redirect_to(signin_path) }
         end
 
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { should redirect_to(signin_path) }
+          it { should redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          it { should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          it { should redirect_to(signin_path) }
         end
       end
     end
@@ -34,12 +46,12 @@ describe "Authentication Requests" do
 
       describe "submitting a GET request to the Users#new action" do
         before { get new_user_path }
-        specify { should redirect_to(root_path) }
+        it { should redirect_to(root_path) }
       end
 
       describe "submitting a POST request to the Users#create action" do
         before { post users_path }
-        specify { should redirect_to(root_path) }
+        it { should redirect_to(root_path) }
       end
     end
 
@@ -50,7 +62,7 @@ describe "Authentication Requests" do
 
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
-        specify { should redirect_to(root_path) }
+        it { should redirect_to(root_path) }
       end
     end
 
@@ -62,7 +74,7 @@ describe "Authentication Requests" do
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
-        specify { should redirect_to(root_path) }
+        it { should redirect_to(root_path) }
       end
     end
 
@@ -73,7 +85,7 @@ describe "Authentication Requests" do
 
       describe "submitting a DELETE request to the Users#destroy action for the admin's own user" do
         before { delete user_path(user) }
-        specify { should redirect_to(root_path) }
+        it { should redirect_to(root_path) }
       end
     end
   end
